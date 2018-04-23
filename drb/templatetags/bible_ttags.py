588e588e73c2efa353cd39f2ce73cd532dfb2848
immensity <orjichidi95@gmail.com>
2018-04-23 21:47:07 +0400

    mc

--- drb/templatetags/bible_ttags.py
+++ drb/templatetags/bible_ttags.py
@@ -31,35 +31,6 @@ def hyperlink_underlined_markdown(string_to_process):
 #     convert_underlines = hyperlink_underlined_markdown(text)
 #     return mark_safe(markdown.markdown(convert_underlines))
 
-
-modal = """
-<a href="" data-toggle="modal" data-target="#Commentary">Here</a>
-
-<div class="modal fade" id="Commentary" tabindex="-1" role="dialog" aria-labelledby="commentaryDetail" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-        
-            <div class="modal-header">
-                <h4 class="modal-title" id="commentaryDetail">{{ commentary.heading }} heading here</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-
-            <div class="modal-body">
-                <p>{{ commentary.text }} some text here</p>
-                
-            </div>
-
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
-"""
-
-
 def hyperlink_underlined(string_to_process):
     """Added href to underline strings"""
 

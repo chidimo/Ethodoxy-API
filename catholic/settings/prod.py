6b3184147d80b420873cc4cdef92273f7dcb9390
immensity <orjichidi95@gmail.com>
2018-10-05 21:47:25 +0100

    whitenoise

--- catholic/settings/prod.py
+++ catholic/settings/prod.py
@@ -9,4 +9,11 @@ DATABASES = {
     }
 }
 
+
+MIDDLEWARE += [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

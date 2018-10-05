371692bea7ba660dd0510effafb88393e66e9e2c
immensity <orjichidi95@gmail.com>
2018-10-05 14:46:36 +0100

    add siteuser

---
+++ catholic/settings/prod.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
---
+++ siteuser/admin.py
@@ -0,0 +1,61 @@
+"""Admin"""
+import pprint
+from django.contrib import admin
+from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import CustomUser, Role, SiteUser, Message, SiteUserPermission
+from .forms import UserChangeForm, UserCreationForm
+
+from django.contrib.sessions.models import Session
+
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ('session_key', '_session_data', 'expire_date')
+
+class SiteUserAdmin(admin.ModelAdmin):
+    list_display = ("pk", "screen_name", "user", "first_name", "last_name", 'all_roles', "slug", "location", "key", "quota", "used", "remaining_quota")
+    # list_editable = ('location', )
+
+    def all_roles(self, obj):
+        return ", ".join([role.name for role in obj.roles.all()])
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('read', 'creator', 'receiver', 'body', 'thread_id')
+    list_editable = ('thread_id', )
+
+class SiteUserPermissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code_name', 'permitted_siteusers')
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'is_admin', 'is_active', "siteuser")
+    list_filter = ('is_admin', )
+    fieldsets = (
+        (None, {'fields' : ('email', 'password')}),
+        ('Personal info', {'fields' : ()}),
+        ('Permissions', {'fields' : ('is_admin', 'is_active')})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes' : ('wide', ),
+            'fields' : ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email', )
+    ordering = ('email', )
+    filter_horizontal = ()
+
+admin.site.register(SiteUser, SiteUserAdmin)
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Role)
+admin.site.register(Permission)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(SiteUserPermission, SiteUserPermissionAdmin)
+
+admin.site.unregister(Group)
+admin.site.register(Session, SessionAdmin)

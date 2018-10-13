951b91564403f720b2618f3c94d0c62aaf73c564
immensity <orjichidi95@gmail.com>
2018-10-13 14:55:08 +0100

    drb single page app completed

--- drb/urls.py
+++ drb/urls.py
@@ -10,9 +10,12 @@ urlpatterns = [
     path('book/<slug:slug>/<int:pk>/', views.BookChapters.as_view(), name='book_detail'),
     path('<str:book_name>-<int:number>/<int:pk>/', views.ChapterDetail.as_view(), name='chapter_detail'),
     path('verse/<int:pk>/', views.VerseDetail.as_view(), name='verse_detail'),
+    
+    path('single-page-view/', views.single_page_view, name='single_page_view'),
+    path('chapter/<int:pk>/', views.get_chapter, name='get_chapter'),
 ]
 
 urlpatterns += [
-    path('commentaries/', views.CommentaryByChapter.as_view(), name='commentaries'),
+    path('challoner/', views.CommentaryByChapter.as_view(), name='commentaries'),
     path('commentary/challoner/<slug:slug>/', views.CommentaryTextDetail.as_view(), name='commentary_detail'),
 ]
--- drb/views.py
+++ drb/views.py
@@ -1,7 +1,25 @@
+from collections import OrderedDict
+
+from django.core import serializers
+
 from django.shortcuts import render
 from django.views import generic
 from pure_pagination.mixins import PaginationMixin
 from .models import Version, Book, Chapter, Verse, Commentary, CommentaryText
+from django.http import JsonResponse, HttpResponse
+
+def single_page_view(request):
+    template = 'drb/single_page_view.html'
+    context = {}
+    context['books'] = Book.objects.all()
+    return render(request, template, context)
+
+def get_chapter(request, pk):
+    return_data = OrderedDict()
+    chapter = Chapter.objects.get(pk=pk)
+    for verse in chapter.verse_set.all():
+        return_data[verse.number] = verse.text
+    return JsonResponse(return_data)
 
 class Index(PaginationMixin, generic.ListView):
     model = Book

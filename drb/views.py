12669c983eebacb957394311ec4cd43fa900edae
Orji Chidi <orjichidi95@gmail.com>
2018-10-13 15:46:56 +0100

    remove old drb link

--- drb/views.py
+++ drb/views.py
@@ -8,8 +8,8 @@ from pure_pagination.mixins import PaginationMixin
 from .models import Version, Book, Chapter, Verse, Commentary, CommentaryText
 from django.http import JsonResponse, HttpResponse
 
-def single_page_view(request):
-    template = 'drb/single_page_view.html'
+def douay_rheims_bible(request):
+    template = 'drb/douay_rheims_bible.html'
     context = {}
     context['books'] = Book.objects.all()
     return render(request, template, context)
@@ -20,11 +20,6 @@ def get_chapter(request, pk):
     for verse in chapter.verse_set.all():
         return_data[verse.number] = verse.text
     return JsonResponse(return_data)
-
-class Index(PaginationMixin, generic.ListView):
-    model = Book
-    template_name = "drb/index.html"
-    context_object_name = "book_list"
 
 class BookChapters(generic.ListView):
     model = Book

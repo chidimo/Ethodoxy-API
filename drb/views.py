6ed0080ea17a7e54294f4274ad0c7b7ab02d6cb1
Orji Chidi <orjichidi95@gmail.com>
2018-10-16 20:07:02 +0100

    remove extraneous files and classes

--- drb/views.py
+++ drb/views.py
@@ -21,39 +21,8 @@ def get_chapter(request, pk):
         return_data[verse.number] = verse.text
     return JsonResponse(return_data)
 
-class BookChapters(generic.ListView):
-    model = Book
-    context_object_name = "book_chapter_list"
-    template_name = "drb/book_chapter_list.html"
-
-    def book(self):
-        return Book.objects.get(pk=self.kwargs['pk'], slug=self.kwargs['slug'])
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['book'] = self.book()
-        return context
-
-    def get_queryset(self):
-        return self.book().chapter_set.all()
-
-class ChapterDetail(generic.DetailView):
-    model = Chapter
-    context_object_name = "chapter"
-    template_name = "drb/chapter.html"
-
-class VerseDetail(generic.DetailView):
-    model = Verse
-    context_object_name = "verse"
-    template_name = "drb/verse.html"
-
 class CommentaryByChapter(PaginationMixin, generic.ListView):
     model = CommentaryText
     template_name = "drb/challoner.html"
     context_object_name = "commentaries"
     paginate_by = 20
-
-class CommentaryTextDetail(generic.DetailView):
-    model = CommentaryText
-    template_name = "drb/commentary_detail.html"
-    context_object_name = "commentary"

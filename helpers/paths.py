a00539bc36fb8b129e25de630a694a18caddd687
Orji Chidi <orjichidi95@gmail.com>
2019-07-21 13:03:28 +0100

    refactor: update paths

--- helpers/paths.py
+++ helpers/paths.py
@@ -7,12 +7,12 @@ import glob
 import django
 from django.conf import settings
 
-BASE_DIR = settings.BASE_DIR
-SAVE_PATH = os.path.join(BASE_DIR, 'drbo_org_scrap')
-DATA_STORE = os.path.join(BASE_DIR, 'drbo_data')
-CHAPTER_STORE = os.path.join(BASE_DIR, 'drbo_data', 'chapters')
-VERSE_STORE = os.path.join(BASE_DIR, 'drbo_data', 'verses')
-CHALLONER_STORE = os.path.join(BASE_DIR, 'drbo_data', 'challoner')
+BASE_DIR = os.path.join(settings.BASE_DIR, 'drbo')
+SAVE_PATH = os.path.join(BASE_DIR, 'text')
+DATA_STORE = os.path.join(BASE_DIR, 'json')
+CHAPTER_STORE = os.path.join(DATA_STORE, 'chapters')
+VERSE_STORE = os.path.join(DATA_STORE, 'verses')
+CHALLONER_STORE = os.path.join(DATA_STORE, 'challoner')
 
 NT = (os.path.join(DATA_STORE, 'new_test.json'))
 OT = (os.path.join(DATA_STORE, 'old_test.json'))
